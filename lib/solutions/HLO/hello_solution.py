719a63403b3095f1458023b8777f464a04916f89
sbx_user1051 <sbx_user1051@169.254.70.185>
2026-03-22 21:03:11 +0000

    Sun Mar 22 21:03:11 UTC 2026

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,11 +4,7 @@ class HelloSolution:
     # friend_name = unicode string
     def hello(self, friend_name: str) -> str:
         """
-        Return a string in the format "Hello, {friend_name}!" where {friend_name} is replaced with the value of the friend_name parameter.
-        :param friend_name: The name of the friend to greet.
-        :return: A greeting string.
+        Return a string which says Hello, World!.
         """
-        if not isinstance(friend_name, str):
-            raise ValueError("friend_name must be a string")
         
-        return f"Hello, {friend_name}!"
+        return f"Hello, World!"
--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -10,4 +10,3 @@ class SumSolution:
         """
 
         return x + y
-
