9b2ff9428eceb53e1ea1099a6452ca8ee01134e1
sbx_user1051 <sbx_user1051@169.254.16.181>
2026-03-22 21:01:58 +0000

    Sun Mar 22 21:01:58 UTC 2026

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,13 @@
 class HelloSolution:
     
     # friend_name = unicode string
-    def hello(self, friend_name):
-        raise NotImplementedError()
+    def hello(self, friend_name: str) -> str:
+        """
+        Return a string in the format "Hello, {friend_name}!" where {friend_name} is replaced with the value of the friend_name parameter.
+        :param friend_name: The name of the friend to greet.
+        :return: A greeting string.
+        """
+        if not isinstance(friend_name, str):
+            raise ValueError("friend_name must be a string")
+        
+        return f"Hello, {friend_name}!"
